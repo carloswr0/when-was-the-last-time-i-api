aac8aa2b343046bcc6fd9f83f7df80bf08ef8852
Carlos Wilthew <carlos.rojas@qapital.com>
2026-04-27 15:59:03 -0300

    Preparation for Vercel deployment

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express, { type Request, type Response } from "express";
+import type { JwtPayload } from "jsonwebtoken";
+import cors from "cors";
+import { sendSuccess } from "./helpers/response.helper.ts";
+import connectToMongoDB from "./config/mongodb.config.ts";
+import healthRouter from "./routers/health.router.ts";
+import authRouter from "./routers/auth.router.ts";
+import authMiddleware from "./middlewares/auth.middleware.ts";
+import groupsRouter from "./routers/groups.router.ts";
+import userRouter from "./routers/user.router.ts";
+
+connectToMongoDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/health", healthRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/groups", authMiddleware, groupsRouter);
+app.use("/api/user", authMiddleware, userRouter);
+app.use("/api/test", authMiddleware, (req: Request, res: Response) => {
+  const { user } = req as Request & { user?: JwtPayload };
+  sendSuccess(
+    res,
+    200,
+    null,
+    `Auth token is valid and active, also the test endpoint is working! ${user?.name ?? "Guest"}`,
+  );
+});
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,9 @@
-import express, { type Request, type Response } from "express";
-import type { JwtPayload } from "jsonwebtoken";
-import cors from "cors";
+import app from "./app.ts";
 import ENVIRONTMENT from "./config/environment.config.ts";
-import { sendSuccess } from "./helpers/response.helper.ts";
-import connectToMongoDB from "./config/mongodb.config.ts";
-import healthRouter from "./routers/health.router.ts";
-import authRouter from "./routers/auth.router.ts";
-import authMiddleware from "./middlewares/auth.middleware.ts";
-import groupsRouter from "./routers/groups.router.ts";
-import userRouter from "./routers/user.router.ts";
 
-connectToMongoDB();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/health", healthRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/groups", authMiddleware, groupsRouter);
-app.use("/api/user", authMiddleware, userRouter);
-app.use("/api/test", authMiddleware, (req: Request, res: Response) => {
-  const { user } = req as Request & { user?: JwtPayload };
-  sendSuccess(
-    res,
-    200,
-    null,
-    `Auth token is valid and active, also the test endpoint is working! ${user?.name ?? "Guest"}`,
-  );
-});
-
-app.listen(ENVIRONTMENT.PORT, () => {
-  console.log("Express server is running on port: ", ENVIRONTMENT.PORT);
-});
+if (!process.env.VERCEL) {
+  const port = ENVIRONTMENT.PORT ?? 3000;
+  app.listen(port, () => {
+    console.log("Express server is running on port: ", port);
+  });
+}
